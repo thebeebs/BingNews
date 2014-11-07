8297cda392298a34c6dc23fadc2ccc4103e92e0e
thebeebs <martinbeeby@hotmail.com>
2014-11-07 17:17:44 +0000

    Added Hero Image and Updating String

--- BingNews/scripts/index.js
+++ BingNews/scripts/index.js
@@ -5,17 +5,27 @@
 (function () {
     "use strict";
 
-    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+    document.addEventListener('deviceready', onDeviceReady.bind(this), false);
 
     function onDeviceReady() {
         // Handle the Cordova pause and resume events
-        document.addEventListener( 'pause', onPause.bind( this ), false );
+        document.addEventListener('pause', onPause.bind(this), false);
         document.addEventListener('resume', onResume.bind(this), false);
+
+        WinJS.Namespace.define("Sample.ListView", {
+            data: new WinJS.Binding.List([])
+        });
 
         var user = "0fac171a-4cc7-49f8-aeb9-b99eaca3de76";
         var pwd = "20gEEQNoCA6h03BU+uaB3ysIPcFvPiZ+PEq06HuakcA=";
         var cryp = window.btoa(user + ":" + pwd);
 
+        document.getElementById("refreshNews").onclick = FetchNewsClick;
+
+        FetchNewsClick();
+
+        function FetchNewsClick() {
+            document.getElementById("status").innerText = "Updating";
         var options = {
             url: "https://api.datamarket.azure.com/Bing/Search/v1/News?Query=%27microsoft%27&Market=%27en-GB%27&Adult=%27Strict%27",
             responseType: "json",
@@ -23,28 +33,45 @@
             headers: {
                 "Authorization": "Basic " + cryp, "Content-type": "application/json; charset=utf-8", "Accept": "application/json"
             },
-            
+
         };
+        function FetchHeroImage(search) {
+            var optionsImage = {
+                url: "https://api.datamarket.azure.com/Bing/Search/v1/Image?Query=%27" + search.Title + "%27",
+                responseType: "json",
+                type: "GET",
+                headers: {
+                    "Authorization": "Basic " + cryp, "Content-type": "application/json; charset=utf-8", "Accept": "application/json"
+                },
 
-        function output(s) {
-            document.getElementById("content").innerHTML += s;
+            };
+            WinJS.xhr(optionsImage).done(
+           function (result) {
+               heroImage.src = result.response.d.results[0].MediaUrl;
+               document.getElementsByTagName("h2")[0].innerText = result.response.d.results[0].Title;
+               document.getElementsByTagName("h3")[0].innerText = result.response.d.results[0].Description;
+               document.getElementById("status").innerText = "Updated";
+           },
+           function (result) {
+              document.getElementById("status").innerText = "Failed";
+           }
+       );
+
         }
-
-        WinJS.Namespace.define("Sample.ListView", {
-            data: new WinJS.Binding.List([])
-        });
 
         WinJS.xhr(options).done(
             function (result) {
-                var feed = JSON.parse(result.responseText);
-                for (var i = 0; i < feed.d.results.length; i++) {
-                    Sample.ListView.data.push(feed.d.results[i]);
-                }             
+                FetchHeroImage(result.response.d.results[0]);
+                for (var i = 1; i < result.response.d.results.length; i++) {
+                    Sample.ListView.data.push(result.response.d.results[i]);
+
+                }
             },
             function (result) {
                 console.log("Fail");
             }
-        );       
+        );
+    }
 
         WinJS.UI.processAll();
 
@@ -57,4 +84,4 @@
     function onResume() {
         // TODO: This application has been reactivated. Restore application state here.
     };
-} )();
+})();
